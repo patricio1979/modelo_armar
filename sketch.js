0dff90beb031d7fd04dd65c84bf50d714aeff7b2
Patricio <scoresmexico@yahoo.com.mx>
2021-03-08 10:42:50 -0600

    Update sketch.js
    canvas and pixelDensity

--- sketch.js
+++ sketch.js
@@ -40,9 +40,9 @@ function preload() {
 }
 
 function setup() {
-  cnv = createCanvas(s_width, s_height);
+  createCanvas(s_width, s_height);
   pixelDensity(1);
-  cnv.position(s_width - (s_width/9), s_height - (s_height/9));
+  position(s_width - (s_width/9), s_height - (s_height/9));
   startT=millis();
   //--- boton comienzo
   botonitoDos = createButton('comenzar');
